bd6e3d397409ed857a8e3d4778e6ece7bb65cd47
maols7022 <maols7022@student.nord.no>
2025-11-03 16:28:22 +0100

    Update App.tsx

--- nordconnect-vite/src/App.tsx
+++ nordconnect-vite/src/App.tsx
@@ -2,9 +2,5 @@ import React from 'react'
 import NordConnect from './NordConnect'
 
 export default function App() {
-  return (
-    <div className="font-sans">
-      <NordConnect />
-    </div>
-  )
+  return <NordConnect />
 }
